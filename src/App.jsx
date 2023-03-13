3d4fd6bcd45b4a0b0436d452f9b17e85ae7deb0e
Matthew Gargano <Matthew.Gargano@csi.cuny.edu>
2023-03-13 19:20:37 -0400

    fast forwarding...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { useSelector } from 'react-redux';
+import Counter from './components/Counter';
 
 function App() {
-  
+  const count = useSelector((state) => {
+    return state.counter.count;
+  });
 
   return (
     <>
-      
+    I KNOW! THE COUNTER IS {count}
+      <Counter />
+      <Counter />
+      <Counter />
     </>
   )
 }
